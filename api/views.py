75124a658f1229b2c2222257d2dc79b9f6c250e0
David <you@example.com>
2019-06-10 19:19:11 +0800

    add request to location, and decode the QR Code

--- api/views.py
+++ api/views.py
@@ -7,6 +7,9 @@ from django.views.decorators.csrf import csrf_exempt
 import cv2
 import numpy
 from datetime import datetime
+import json
+import requests
+from pyzbar import pyzbar
 # from PIL import Image
 
 # Create your views here.
@@ -28,10 +31,22 @@ def images(request):
     # return HttpResponse("oh my god")
     with open("api/log.txt","a+") as f:
         f.write('\r\nbegin time %s \r' % datetime.now())
+        key = 'AXLBZ-O5IL3-GTM3T-3FBKS-CXSVZ-FVF4X'
+        try:
+            lat = request.POST.get('lat')
+            log = request.POST.get('log')
+            url = 'https://apis.map.qq.com/ws/geocoder/v1/?location='+lat+','+log+'&key='+key+'&get_poi=1'
+            a = requests.get(url)
+            results = json.loads(a.text)
+            location = results['result']['address']
+        except Exception as ex:
+            location = '无法获取位置信息'
         try:
             # pic = request.FILES['file']
             im_pic = cv2.imdecode(numpy.fromstring(request.FILES['file'].read(), numpy.uint8), cv2.IMREAD_UNCHANGED)
-
+            # im_bar = cv2.imread(request.FILES['file'],0)
+            barcode = pyzbar.decode(im_pic)
+            url = barcode[0].data.decode('utf-8')
             # image = cv2.imread(file)
             # im_pic = Image.open(pic)
             img2gray = cv2.cvtColor(im_pic, cv2.COLOR_BGR2GRAY)
@@ -41,11 +56,11 @@ def images(request):
             # abc = type(file)
             # abc = str(abc)
             if imageVar > 300:
-                f.write("clear")
-                return HttpResponse('图片清晰'+str(imageVar))
+                f.write("clear"+location + ',url is: '+url)
+                return HttpResponse('图片清晰'+str(imageVar) + "位置是：" +location)
             else:
-                f.write("unclear")
-                return HttpResponse('图片不清晰，请再上传一次'+str(imageVar))
+                f.write("unclear"+location + ',url is: '+url)
+                return HttpResponse('图片不清晰，请再上传一次'+str(imageVar) + "位置是：" + location)
         except KeyError:
             # return Response({'data':'not found'})
             f.write("no image")
